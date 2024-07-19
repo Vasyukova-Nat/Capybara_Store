907d35797b5b95c76b2dbc24315a941d45134e35
Васюкова Наталья Павловна <144936520+Vasyukova-Nat@users.noreply.github.com>
2024-07-19 17:21:29 +0300

    V7, +try-catch block in deviceController

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,17 +1,22 @@
 const uuid = require('uuid')  // пакет для генерирования рандомных уникальных id
 const path = require('path');  // модуль для расположения файла изображения
 const {Device, DeviceInfo} = require('../models/models')  // импорт модели девайса для БД
+const ApiError = require('../error/ApiError');
 
 class DeviceController {
-    async create(req, res) {  // добавление девайса
-        const {name, price, brandId, typeId, info} = req.body  // название, цена, id бренда, id типа и характеристики
-        const {img} = req.files  // фото устройства. Для него нужно установить пакет - npm i express-fileupload
-        let fileName = uuid.v4() + ".jpg"  // нужно для файла сгенерировать уникальное имя. Для этого установим пакет - npm i uuid. Он будет генерировать случайные id.
-        img.mv(path.resolve(__dirname, '..', 'static', fileName))  // переместить файл в папку static
-        
-        const device = await Device.create({name, price, brandId, typeId, img: fileName});  // создать девайс
+    async create(req, res, next) {  // добавление девайса
+        try {
+            const {name, price, brandId, typeId, info} = req.body  // название, цена, id бренда, id типа и характеристики
+            const {img} = req.files  // фото устройства. Для него нужно установить пакет - npm i express-fileupload
+            let fileName = uuid.v4() + ".jpg"  // нужно для файла сгенерировать уникальное имя. Для этого установим пакет - npm i uuid. Он будет генерировать случайные id.
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))  // переместить файл в папку static
+            
+            const device = await Device.create({name, price, brandId, typeId, img: fileName});  // создать девайс
 
-        return res.json(device)
+            return res.json(device)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
     }
 
     async getAll (req, res) {
